d1d3840ca9c284aa06a0d1f174d9f6cfcb6b9e1d
jsbuddy <judecodes@gmail.com>
2020-09-12 01:07:28 +0100

    feat: add toolbar to setting fragment

--- app/src/main/java/com/example/gadsleaderboard/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/ui/settings/SettingsFragment.kt
@@ -1,12 +1,33 @@
 package com.example.gadsleaderboard.ui.settings
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.gadsleaderboard.R
 
 class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = super.onCreateView(inflater, container, savedInstanceState)!!
+        setupToolbar(root)
+        return root
+    }
+
+    private fun setupToolbar(root: View) {
+        val toolbar = root.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Settings"
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+        toolbar.setNavigationOnClickListener { requireActivity().onBackPressed() }
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
